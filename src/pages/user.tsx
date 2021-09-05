040d55ecb190cfbf9dfdbec243b038fb929a6a5b
pillar <ymblender@gmail.com>
2021-09-06 03:14:26 +0800

    fix: remove getServerSideProps

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,12 +1,3 @@
 export default function User(props: Record<string, unknown>) {
-  return <div>ssr props2: {JSON.stringify(props)}</div>;
+  return <div>ssr props: {JSON.stringify(props)}</div>;
 }
-export const getServerSideProps = async (query: Record<string, unknown>) => {
-  console.log("get");
-  const str = await fetch("/ping", { method: "GET" }).then((v) => v.text());
-  return {
-    beforeFech: "ping: " + str,
-    date: new Date().toString(),
-    query: query,
-  };
-};
